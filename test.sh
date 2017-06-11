027b9c74094aab06f19947689457f63498e0066c
damien clochard <damien@dalibo.info>
2017-06-11 11:02:56 +0200

    [ci] remove Makefile

--- test.sh
+++ test.sh
@@ -1,43 +1,62 @@
 #!/bin/bash
 
+# $1 is required 
+# Should be a major postgresql version
+# i.e. : 7.4, 8.0, 8.1, 8.2, 8.3, 8.4, 9.0, 9.1, 9.2, 9.3, 9.4 , 9.5, 9.6, 10
+
 HOST=localhost
 STATUS_FILE=/tmp/check_pgactivity.data
 C=`dirname $0`/check_pgactivity 
 
-#latest: 9.6
-#9.6: 9.5 
-#9.5: 9.4
-#9.4: 9.3
-#9.3: 9.2
-#9.2: 9.1
-#9.1: 9.0
-#9.0: 8.4
-#8.4: 8.3
-#8.3: 8.2 
-#8.2: 8.1 last_vacuum last_analyze backends_status
-#8.1: 8.0 autovacuum
-#8.0: 7.4
-#7.4: all
+#
+# Put your all-version services below 
+#
 
-#all: archive_folder backends
 
-# archive_folder      
-#$C -s archive_folder --path /var/lib/postgres/pg_xlog
+# archive_folder
+#$C -s archive_folder --path /var/lib/postgresql/$1/main/pg_xlog || exit 1
 
-$C -s backends -h $HOST -w '10%' -c '30%'
-
-$C -s backends_status -h $HOST -w 'waiting=5,idle_xact=10' -c 'waiting=20,idle_xact=30'
+# backends
+$C -s backends -h $HOST -w '10%' -c '30%' || exit 1
 
 [[ "$1" == "7.4" ]] &&  exit 0
- 
+
+#
+# Put your 8.0+ servicess below 
+# 
+
 [[ "$1" == "8.0" ]] &&  exit 0
 
-#autovacuum:
-$C -s autovacuum -h $HOST
+#
+# Put your 8.1+ servicess below 
+#
+
+# autovacuum:
+$C -s autovacuum -h $HOST || exit 1
+
+# backup_label_age
+$C -s backup_label_age -h $HOST -w 1h30m25s -c 28h30m || exit 1
+
+[[ "$1" == "8.1" ]] &&  exit 0
+# Put your 8.2+ servicess below 
+
+# last_analyze
+$C -s last_analyze --status-file /tmp/check_pgactivity.data -h $HOST -w 30m -c 1h30m || exit 1
+
+# last_vacuum
+$C -s last_vacuum --status-file /tmp/check_pgactivity.data  -h $HOST -w 30m -c 1h30m || exit 1
+
+# backend_status
+$C -s backends_status -h $HOST -w 'waiting=5,idle_xact=10' -c 'waiting=20,idle_xact=30' || exit 1
 
 
+[[ "$1" == "8.2" ]] &&  exit 0
 
-$C -s last_analyze --status-file /tmp/check_pgactivity.data -h $HOST -w 30m -c 1h30m
-$C -s last_vacuum --status-file /tmp/check_pgactivity.data  -h $HOST -w 30m -c 1h30m
+#
+# Put your 8.3+ servicess below 
+#
 
- 
+# bgwriter
+$C -s bgwriter --status-file $STATUS_FILE -h $HOST || exit 1
+$C -s bgwriter --status-file $STATUS_FILE -h $HOST -w 15% -c 33% || exit 1
+
